dbbb60fb255bfcc221c6afbeda0ac95475cdafbc
Thiago Silva <thiago2b9s@gmail.com>
2023-04-12 11:48:31 -0300

    wak waka

--- AthleticaMM/Login/urls.py
+++ AthleticaMM/Login/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from Login.views import Erro, Register, ForgotPassword, Logado, PaginaLogin
-
+from .views import Index
 
 urlpatterns = [
     path('logado/', Logado, name='logado'),
     path('erro/', Erro, name='pagina de erro'),
     path('registro/', Register, name='pagina de cadastro'),
     path('EsqueciSenha/', ForgotPassword, name='esqueci a senha'),
-    path('login/', PaginaLogin, name='pagina de login')
+    path('', Index.as_view(), name='pagina de login'),
 ]
